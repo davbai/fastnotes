77bb80dac8ac42fd2234be973b96af25599b7765
David <baidavd@gmail.com>
2014-01-10 22:29:46 -0800

    Moved html to index.php

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<?php include_once("index.html"); ?>
+<!-- for now using index.php for Heroku deployment -->
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>fastnotes</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+    <body lang="en">
+        <div id="metabar">
+            <span id="char-count" class="metabar-item">0</span>
+            <input id="download-btn" type="image" class="metabar-item metabar-btn" src="img/download.png" alt="Download Note">
+        </div>
+        <div id="note-container">
+            <div id="note-title" contenteditable="true" data-default-value="Untitled"></div>
+            <div id="note-body" contenteditable="true" data-default-value="Write your note.."></div>
+        </div>
+
+        <script type="text/javascript" src="js/libs/jquery-2.0.3.min.js"></script>
+        <script type="text/javascript" src="js/libs/FileSaver.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+    </body>
+</html>
